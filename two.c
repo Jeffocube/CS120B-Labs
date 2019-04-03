bf2665493d5c7e24cdb6f88e0801a1fd7b292123
Jeffery <jlai030@ucr.edu>
2019-04-03 15:57:15 +0000

    Fixed

--- two.c
+++ two.c
@@ -1,12 +1,19 @@
 #include <avr/io.h>
 
 int main(void){
-    DDRA = 0x0; PORTA = 0xF;
-    DDRC = 0xF; PORTC = 0x0;
-    unsigned char tempC = 0x0;
+    DDRA = 0x00; PORTA = 0xFF;
+    DDRC = 0xFF; PORTC = 0x00;
+    unsigned char tempC = 0x00;
     while(1){
+        tempC = PINA & 0x0F;   
         //switch
-        tempC = PINA & 0xF;   	
+        switch(tempC) {
+            case 0x0F :
+                tempC = 0x8F;
+                break;
+            default :
+                break;
+        }
         //set
         PORTC = tempC;    
         
