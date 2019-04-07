2b9995011dcf54c024e8770c49200c31d06c51ac
Jeffocube <30770176+Jeffocube@users.noreply.github.com>
2019-04-07 02:42:50 -0700

    Update one.c

--- one.c
+++ one.c
@@ -1,3 +1,14 @@
+/*	Partner 1 Name & E-mail:Jeffery Lai
+ *	Partner 2 Name & E-mail:n/a
+ *	Lab Section: 025
+ *	Assignment: Lab # 2 Exercise # 1
+ *	Exercise Description: [optional - include for your own benefit]
+ *	
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+
+
 #include <avr/io.h>
 
 int main(void){
